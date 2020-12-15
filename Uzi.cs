75496dfe3601861662fcf9650aec7e40cc6a81a4
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-12-16 04:36:11 +0700

    Add files via upload

---
+++ Uzi.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Uzi : Weapon
+{
+    public override void Shoot(Transform shootPoint)
+    {
+        CreateBullet(shootPoint.position);
+        CreateBullet(new Vector3(shootPoint.position.x - 0.3f, shootPoint.position.y));
+    }
+    
+    private void CreateBullet(Vector3 position)
+    {
+        Instantiate(Bullet, position, Quaternion.identity);
+    }
+}
