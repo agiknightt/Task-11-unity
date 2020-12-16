77ce37dabe307d650787cc20452efa344faec16d
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-12-16 17:12:48 +0700

    Add files via upload

--- Uzi.cs
+++ Uzi.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Uzi : Weapon
 {
+    private float _offset = -0.3f;
     public override void Shoot(Transform shootPoint)
-    {
+    {        
         CreateBullet(shootPoint.position);
-        CreateBullet(new Vector3(shootPoint.position.x - 0.3f, shootPoint.position.y));
+        CreateBullet(new Vector3(shootPoint.position.x - _offset, shootPoint.position.y));
     }
     
     private void CreateBullet(Vector3 position)
